f8a10cb6edd710b3e4300eef5b3364912ddd86fa
fhammer <fhammer@qq.com>
2017-04-16 23:54:14 +0800

    init

---
+++ app/src/main/java/play/fzh/bj/notepad/util/SizeUtil.java
@@ -0,0 +1,30 @@
+package play.fzh.bj.notepad.util;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * 尺寸转换工具类
+ */
+
+public class SizeUtil {
+    //dpi转px
+    public static int Dp2Px(Context context, int dpi) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpi, context.getResources().getDisplayMetrics());
+    }
+
+    //px转dp
+    public static int Px2Dp(Context context, int px) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, px, context.getResources().getDisplayMetrics());
+    }
+
+    //sp转px
+    public static int Sp2Px(Context context, int sp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics());
+    }
+
+    //px转sp
+    public static int Px2Sp(Context context, int px) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, px, context.getResources().getDisplayMetrics());
+    }
+}
